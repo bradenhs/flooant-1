8b4547423850a4d44db47573751874d21cf90c46
Braden Snell <braden@snells-mbp.attlocal.net>
2018-02-22 09:56:58 -0500

    experiments with status bar

--- client/main/App.tsx
+++ client/main/App.tsx
@@ -5,7 +5,10 @@ import {
   View,
   TouchableOpacity,
   Text,
-  Button
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  StatusBar
 } from "react-native";
 
 const divide = 60;
@@ -13,87 +16,96 @@ const divide = 60;
 export class App extends React.Component {
   render() {
     return (
-      <KeyboardAvoidingView
-        behavior="padding"
-        style={{ flex: 1, alignItems: "center" }}
+      <TouchableWithoutFeedback
+        onPress={() => {
+          console.log("here");
+          Keyboard.dismiss();
+        }}
+        style={{ flex: 1 }}
       >
-        <View
-          style={{
-            backgroundColor: "#4fc37d",
-            position: "absolute",
-            left: 0,
-            right: 0,
-            top: 0,
-            height: divide + "%"
-          }}
+        <KeyboardAvoidingView
+          behavior="padding"
+          style={{ flex: 1, alignItems: "center" }}
         >
+          <StatusBar barStyle="light-content" />
           <View
             style={{
-              width: 300,
-              alignSelf: "center",
+              backgroundColor: "#3fb36d",
               position: "absolute",
-              bottom: 60
+              left: 0,
+              right: 0,
+              top: 0,
+              height: divide + "%"
             }}
           >
-            <TextInput
-              keyboardType="email-address"
-              placeholder="Email"
-              iconName="email-open"
-            />
-            <View style={{ marginTop: 10 }}>
-              <TextInput
-                placeholder="Password"
-                iconName="lock"
-                secureTextEntry
-              />
-            </View>
-          </View>
-        </View>
-        <View
-          style={{
-            position: "absolute",
-            left: 0,
-            right: 0,
-            top: divide + "%",
-            bottom: 0,
-            backgroundColor: "#4d4d4d",
-            paddingTop: 40
-          }}
-        >
-          <TouchableOpacity
-            onPress={() => 0}
-            style={{
-              backgroundColor: "#4fc37d",
-              elevation: 1,
-              shadowColor: "#444",
-              width: 200,
-              alignSelf: "center",
-              shadowOpacity: 0.3,
-              flexDirection: "row",
-              borderTopColor: "rgba(255,255,255,.3)",
-              borderTopWidth: 1,
-              shadowRadius: 4,
-              marginBottom: 10,
-              shadowOffset: { width: 0, height: 2 },
-              borderRadius: 30
-            }}
-          >
-            <Text
+            <View
               style={{
-                color: "#fff",
-                lineHeight: 54,
-                fontSize: 18,
-                fontWeight: "900",
-                width: "100%",
-                textAlign: "center"
+                width: 300,
+                alignSelf: "center",
+                position: "absolute",
+                bottom: 60
               }}
             >
-              LOGIN
-            </Text>
-          </TouchableOpacity>
-          <Button title="Sign Up" onPress={() => 0} color="#fff" />
-        </View>
-      </KeyboardAvoidingView>
+              <TextInput
+                keyboardType="email-address"
+                placeholder="Email"
+                iconName="email-open"
+              />
+              <View style={{ marginTop: 10 }}>
+                <TextInput
+                  placeholder="Password"
+                  iconName="lock"
+                  secureTextEntry
+                />
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              top: divide + "%",
+              bottom: 0,
+              backgroundColor: "#4d4d4d",
+              paddingTop: 40
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => 0}
+              style={{
+                backgroundColor: "#4fc37d",
+                elevation: 1,
+                shadowColor: "#444",
+                width: 200,
+                alignSelf: "center",
+                shadowOpacity: 0.3,
+                flexDirection: "row",
+                borderTopColor: "rgba(255,255,255,.3)",
+                borderTopWidth: 1,
+                shadowRadius: 4,
+                marginBottom: 10,
+                shadowOffset: { width: 0, height: 2 },
+                borderRadius: 30
+              }}
+            >
+              <Text
+                style={{
+                  color: "#fff",
+                  lineHeight: 54,
+                  fontSize: 18,
+                  fontWeight: "900",
+                  width: "100%",
+                  textAlign: "center"
+                }}
+              >
+                LOGIN
+              </Text>
+            </TouchableOpacity>
+            <Button title="Sign Up" onPress={() => 0} color="#fff" />
+          </View>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     );
   }
 }
